176bdbafac24f6b7c16b34cf9d1863d7903844bc
Kcruz04 <kencruz515@gmail.com>
2023-04-09 16:10:23 -1000

    User chaining

--- user.py
+++ user.py
@@ -6,6 +6,7 @@ class User:
         self.age = age
         self.is_rewards_member = False
         self.gold_card_points = 0
+
     def display_info(self):
         print(self.first_name)
         print(self.last_name)
@@ -13,6 +14,8 @@ class User:
         print(self.age)
         print(self.is_rewards_member)
         print(self.gold_card_points)
+        return self
+
     def enroll(self):
         self.is_rewards_member = True
         self.gold_card_points = 200
@@ -24,14 +27,15 @@ class User:
         #     return True
     def spend_points(self, amount):
         self.gold_card_points = self.gold_card_points - amount
+        return amount
 
 user1 = User( 'Kenneth', 'Cruz', 'Kc@email.com', 30)
 user2 = User('Shari', 'J', 'SJ@email.com', 21)
+
+user1.display_info().enroll().spend_points(50)
+user2.enroll().spend_points(80)
 user1.display_info()
 user2.display_info()
-user1.enroll()
-user1.spend_points(50)
-user2.enroll()
-user2.spend_points(80)
-user1.display_info()
-user2.display_info()
+# user2
+# user1.display_info()
+# user2.display_info()
